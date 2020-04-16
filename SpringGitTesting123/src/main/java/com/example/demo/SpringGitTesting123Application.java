4ff85f463f72ea24b1868a84ea916881b47d7927
Salay <Salay@DESKTOP-OP6TRC9>
2020-04-16 23:26:19 +0700

    xxxx

---
+++ SpringGitTesting123/src/main/java/com/example/demo/SpringGitTesting123Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitTesting123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitTesting123Application.class, args);
+	}
+	
+	private void Test123() {
+		System.out.print("HelloWorld");
+	}
+
+}
