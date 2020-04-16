c5cb71f220ac034abd7ad057fdfc3579881937bd
Salay <Salay@DESKTOP-OP6TRC9>
2020-04-16 23:30:01 +0700

    ccc

--- SpringGitTesting123/src/main/java/com/example/demo/SpringGitTesting123Application.java
+++ SpringGitTesting123/src/main/java/com/example/demo/SpringGitTesting123Application.java
@@ -21,8 +21,8 @@ public class SpringGitTesting123Application {
 	private void Test789() {
 		System.out.print("HelloWorld");
 	}
-	
-			private void Test111() {
+
+	private void Test111() {
 		System.out.print("HelloWorld");
 	}
 }
